cfadb500e6cd3040ba7c78c1c23e4944dd90cdda
Colin S <csmail1290@gmail.com>
2018-05-03 15:55:20 -0700

    2 steps forward, 3 steps backwards

--- liri.js
+++ liri.js
@@ -9,3 +9,20 @@ var keys = require('./keys.js');
 // From the instructions
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var action = process.argv[2];
+// onyl the second time I have attempted to use switch for node cli arguments might be buggy
+switch (action) {
+    case 'my-tweets':
+        myTweets();
+        break;
+    case 'spotify-this-song':
+        spotifyThis();
+        break;
+    case 'movie-this':
+        omdb(value);
+        break;
+    case 'do-what-it-says':
+        doWhatItSays();
+        break;
+}
