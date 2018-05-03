4f661101cb094f17a6c536bc5344086c09443556
Colin S <csmail1290@gmail.com>
2018-05-03 15:31:30 -0700

    cleaned it out

--- liri.js
+++ liri.js
@@ -1,56 +1,8 @@
 // enviroment variables can be set with the dotenv package
-var enVar = require("dotenv").config();
+require("dotenv").config();
 
 var fs = require("fs");
-
-
-// The code will import keys from keys.js file 
-fs.readFile("keys.js", "utf8", function(error, data) {
-  // If the code experiences any errors it will log the error to the console.
-  if (error) {
-    return console.log(error);
-  }
-  console.log(data);
-
-  // Then split it by commas (to make it more readable)
-  var apiKeysArr = data.split(",");
-
-  // We will then re-display the content as an array for later use.
-  console.log(apiKeysArr);
-
-});
-var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+var Twitter = require('twitter'); 
 var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
 var request = require('request');
-    request('http://www.google.com', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-});
+var keys = require('./keys.js');    
