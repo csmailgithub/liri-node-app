356f98dc41b7b3abcc9211b1bbfe080604e87f3f
Colin S <csmail1290@gmail.com>
2018-05-03 14:45:29 -0700

    crlf to lf in keys and liri

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.QHMpktKtXHqKGoSmUhVcmUjyj,
+  consumer_secret: process.env.wXxgq6TpISuICnREZZ6dOc4Rg9a8MQ8iND24cfe6IK6XiCDDcv
+,
+  access_token_key: process.env.992136674434936832-RYYqpuk9dMtSaJQtHfGwV1HGrW60XKi,
+  access_token_secret: process.env.dn7unSrAomVGdW5CKKJcCqxQQEjsdpkU5hx8UMLjSHBCG
 };
 
 exports.spotify = {
